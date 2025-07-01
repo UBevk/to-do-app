2da212b468633f3bf85c5c21dae56dd294d971b2
UBevk <125929632+UBevk@users.noreply.github.com>
2025-07-02 01:35:22 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,14 +11,10 @@ app.use(express.json());
 
 /*mongoose.connect('mongodb://localhost:27017/tasks');*/
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('MongoDB connected');
-}).catch((err) => {
-  console.error('MongoDB connection error:', err);
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 
 
 const UserSchema = new mongoose.Schema({
